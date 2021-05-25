7cdfe230d4445d095847a747098d71b7a6af80b4
Nazimcpdd <84698719+Nazimcpdd@users.noreply.github.com>
2021-05-25 16:06:38 +0530

    Update login.js

--- login.js
+++ login.js
@@ -7,8 +7,8 @@ var uiConfig = {
           //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
           //firebase.auth.GithubAuthProvider.PROVIDER_ID,
           firebase.auth.EmailAuthProvider.PROVIDER_ID,
-		  firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-		  firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+		  //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+		  //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
           //firebase.auth.PhoneAuthProvider.PROVIDER_ID
   ],
   // Terms of service url.
